56b6b5394967952fa8ded4cb92455a97802d902f
Jian Chen <cjian@microsoft.com>
2022-11-22 10:36:55 -0800

    Adding time delta to batch file

--- build.bat
+++ build.bat
@@ -2,5 +2,26 @@
 :: Licensed under the MIT License.
 
 @echo off
+
+rem Get start time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+   set /A "start=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
 rem Requires a python 3.6 or higher install to be available in your PATH
 python %~dp0\tools\ci_build\build.py --build_dir %~dp0\build\Windows %*
+
+rem Get end time:
+for /F "tokens=1-4 delims=:.," %%a in ("%time%") do (
+   set /A "end=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)*100+1%%d %% 100"
+)
+
+rem Get elapsed time:
+set /A elapsed=end-start
+
+rem Show elapsed time:
+set /A hh=elapsed/(60*60*100), rest=elapsed%%(60*60*100), mm=rest/(60*100), rest%%=60*100, ss=rest/100, cc=rest%%100
+if %mm% lss 10 set mm=0%mm%
+if %ss% lss 10 set ss=0%ss%
+if %cc% lss 10 set cc=0%cc%
+echo %hh%:%mm%:%ss%,%cc%
