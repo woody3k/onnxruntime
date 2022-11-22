0b8835501d065aedb13509159ff110351c74c3da
Jian Chen <cjian@microsoft.com>
2022-11-22 10:04:20 -0800

    caculating duration for the build

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# Intitializing time in second
+SECONDS=0
+
 # Get directory this script is in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OS=$(uname -s)
@@ -20,3 +23,7 @@ fi
 
 #requires python3.6 or higher
 python3 $DIR/tools/ci_build/build.py --build_dir $DIR/build/$DIR_OS "$@"
+
+#Echo duration for the build
+duration=$SECONDS
+echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
